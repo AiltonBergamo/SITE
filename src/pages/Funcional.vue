21a34e0702306b058aa1cad44cea891c2420ed2b
DevBDS003 <dev003@infobds.com.br>
2024-01-03 09:30:16 -0300

    Refatoração melhoria visual menu funcionalidades

--- src/pages/Funcional.vue
+++ src/pages/Funcional.vue
@@ -13,12 +13,12 @@
             <img src="../assets/icon/address-card-regular.svg" alt="" class="svg_preto">
           </div>
           <h3>Cadastros</h3>
-          <p>Cadastre clientes, funcionarios e fornecedores em um só lugar.</p>
+          <p>Cadastre clientes, funcionários e fornecedores em um só lugar.</p>
         </a>
         <a data-anime="left" class="card"
           href="/funcional/estoque">
           <div class="imag">
-            <img src="../assets/icon/estoque.png" alt="">
+            <img src="../assets/icon/estoque.svg" alt="">
           </div>
           <h3>Estoque</h3>
           <p>Automatize a entrada de produtos em seu estoque e tenha um bom gerenciamento.</p>
@@ -26,25 +26,25 @@
         <a data-anime="left" class="card"
           href="/funcional/frenteCaixa">
           <div class="imag">
-            <img src="../assets/icon/store-solid.svg" alt="" class="svg_preto">
+            <img src="../assets/icon/loja.svg" alt="" class="svg_preto">
           </div>
           <h3>Frente de Caixa</h3>
           <p>Controle de caixa e lançamento de vendas.</p>
         </a>
+      </div>
+      <div class="row">
         <a data-anime="left" class="card" 
           href="/funcional/venda">
           <div class="imag">
-            <img src="../assets/icon//venda.png" alt="">
+            <img src="../assets/icon//carrinhoMercado.svg" alt="">
           </div>
           <h3>Venda</h3>
           <p>Tenha um grande parceiro no controle de seus negócios e em suas vendas. </p>
         </a>
-      </div>
-      <div class="row">
         <a data-anime="left" class="card" target="_blank"
           href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
           <div class="imag">
-            <img src="../assets/icon/file-invoice-dollar-solid.svg" alt="" class="svg_preto">
+            <img src="../assets/icon/notaFiscal.svg" alt="" class="svg_preto">
           </div>
           <h3>Nota Fiscal</h3>
           <p>Emita notas fiscas de forma simplificada.</p>
@@ -55,7 +55,7 @@
         <a data-anime="right" class="card" target="_blank"
           href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Financeiro!">
           <div class="imag">
-            <img src="../assets/icon/financeiro.png" alt="">
+            <img src="../assets/icon/porquinho.svg" alt="">
           </div>
           <h3>Financeiro</h3>
           <p>Controle de forma eficaz suas operações financeiras.</p>
@@ -63,10 +63,12 @@
                 <a href="pagread.html" class= "btn" rel="next" target= "_self">Saiba mais</a>
               </div> -->
         </a>
+      </div>
+      <div class="row">
         <a data-anime="left" class="card" target="_blank"
           href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
           <div class="imag">
-            <img src="../assets/icon/file-invoice-solid.svg" alt="" class="svg_preto">
+            <img src="../assets/icon/codigoBarra.svg" alt="" class="svg_preto">
           </div>
           <h3>Boleto</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -77,7 +79,7 @@
         <a data-anime="left" class="card" target="_blank"
           href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
           <div class="imag">
-            <img src="../assets/icon/link-solid.svg" alt="" class="svg_preto">
+            <img src="../assets/icon/sacoDinheiro.svg" alt="" class="svg_preto">
           </div>
           <h3>Link de Pagamento</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
@@ -96,7 +98,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz@6..12&display=swap');
 .center{
   align-items: center;
   justify-content: center;
@@ -109,6 +112,9 @@ export default {
 .column{
   display: flex;
   flex-direction: column;
+}
+p{
+  font-family: 'Nunito Sans', sans-serif;
 }
 a{
   text-decoration: none;
@@ -127,7 +133,7 @@ a{
   width: 100%;
   height: 68vh;
   border-radius: 20px 60px 35px;
-  background: var(--fundo-padrao);
+  background: white;
   width: 100%;
   height: 100%;  
 }
@@ -158,9 +164,11 @@ a{
 }
 
 .imag{
-  height: 50%;
+  align-self: center;
+  width: 20%;
+  height: 20%;
   display: flex;
-  background-color: white;
+  background-color: #b7d3ff;
   justify-content: center;
   align-items: center;
   margin-top: 1em;
@@ -170,11 +178,17 @@ a{
   margin: 10px;
   display: flex;
   flex-direction: column;
-  background-color: white;  
-  width: 180px;
-  height: 300px;
+  background-color: #b7d3ff;  
+  width: 320px;
+  height: 200px;
   border-radius: 10px;
 }
-
-
+.card:hover p{
+  color: var(--texto-titulo);
+  transition: 1s;
+}
+.card img{
+  padding: 0px;
+  width: auto;
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -24,39 +24,30 @@
       <a data-anime="left" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Estoque!">
         <div class="imag">
-          <img src="../assets/icon/estoque.png" alt="">
+          <!-- <img src="../assets/icon/estoque.png" alt=""> -->
         </div>
         <h3>Estoque</h3>
         <p>Automatize a entrada de produtos em seu estoque e tenha um bom gerenciamento.</p>
-        <!-- <div class="button">
-                <a href="pagread.html" class= "btn" rel="next" target= "_self">Saiba mais</a>
-              </div> -->
       </a>
       <a data-anime="right" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Financeiro!">
         <div class="imag">
-          <img src="../assets/icon/financeiro.png" alt="">
+          <!-- <img src="../assets/icon/financeiro.png" alt=""> -->
         </div>
         <h3>Financeiro</h3>
         <p>Controle de forma eficaz suas operações financeiras.</p>
-        <!-- <div class="button">
-              <a href="pagread.html" class= "btn" rel="next" target= "_self">Saiba mais</a>
-            </div> -->
       </a>
       <a data-anime="left" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
         <div class="imag">
-          <img src="../assets/icon//venda.png" alt="">
+          <!-- <img src="../assets/icon//venda.png" alt=""> -->
         </div>
         <h3>Venda</h3>
         <p>Tenha um grande parceiro no controle de seus negócios e em suas vendas. </p>
-        <!-- <div class="button">
-              <a href="pagread.html" class= "btn" rel="next" target= "_self">Saiba mais</a>
-            </div> -->
       </a>
       <a data-anime="left" class="card" href="/funcional/cadastro">
         <div class="imag">
-          <img src="../assets/icon/address-card-regular.svg" alt="" class="svg_preto">
+          <!-- <img src="../assets/icon/address-card-regular.svg" alt="" class="svg_preto"> -->
         </div>
         <h3>Cadastros</h3>
         <p>Cadastre clientes, funcionarios e fornecedores em um só lugar.</p>
@@ -64,7 +55,7 @@
       <a data-anime="left" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Estoque!">
         <div class="imag">
-          <img src="../assets/icon/store-solid.svg" alt="" class="svg_preto">
+          <!-- <img src="../assets/icon/store-solid.svg" alt="" class="svg_preto"> -->
         </div>
         <h3>Frente de Caixa</h3>
         <p>Controle de caixa e lançamento de vendas.</p>
@@ -72,7 +63,7 @@
       <a data-anime="left" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
         <div class="imag">
-          <img src="../assets/icon/file-invoice-dollar-solid.svg" alt="" class="svg_preto">
+          <!-- <img src="../assets/icon/file-invoice-dollar-solid.svg" alt="" class="svg_preto"> -->
         </div>
         <h3>Nota Fiscal</h3>
         <p>Emita notas fiscas de forma simplificada.</p>
@@ -80,7 +71,7 @@
       <a data-anime="left" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
         <div class="imag">
-          <img src="../assets/icon/file-invoice-solid.svg" alt="" class="svg_preto">
+          <!-- <img src="../assets/icon/file-invoice-solid.svg" alt="" class="svg_preto"> -->
         </div>
         <h3>Boleto</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -88,7 +79,7 @@
       <a data-anime="left" class="card" target="_blank"
         href="https://api.whatsapp.com/send?phone=5544999615259&text=Gostaria de saber a respeito do sistema de gestão de Venda!">
         <div class="imag">
-          <img src="../assets/icon/link-solid.svg" alt="" class="svg_preto">
+          <!-- <img src="../assets/icon/link-solid.svg" alt="" class="svg_preto"> -->
         </div>
         <h3>Link de Pagamento</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
